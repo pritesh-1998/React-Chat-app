5d7cf1ddfcbb85ac768025504a05010e01115df8
Pritesh lathia <72033803+pritesh-1998@users.noreply.github.com>
2024-07-17 23:44:41 +0530

    added the center component and inserted users list

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,7 +1,24 @@
 import "./chat.css";
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Max Verstappen</span>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
+    </div>
   )
 }
 
--- src/components/list/Chatlist/Chatlist.jsx
+++ src/components/list/Chatlist/Chatlist.jsx
@@ -1,17 +1,87 @@
 import { useState } from "react";
 import "./chatlist.css";
 const Chatlist = () => {
-    const[addmod,setaddmod] = useState();
+    const [addmod, setaddmod] = useState();
     return (
         <div className='chatlist'>
             <div className="search">
                 <div className="searchbar">
                     <img src="./search.png" alt="" />
-                    <input type="text" placeholder="search"/>
+                    <input type="text" placeholder="search" />
                 </div>
-                <img src={addmod ? "./minus.png": "./plus.png" } alt="" className="add" onClick={()=>{
+                <img src={addmod ? "./minus.png" : "./plus.png"} alt="" className="add" onClick={() => {
                     setaddmod((prev) => !prev)
                 }} />
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Lewis Hamilton</span>
+                    <p>Hello Good Morning</p>
+                </div>
             </div>
         </div>
     )
