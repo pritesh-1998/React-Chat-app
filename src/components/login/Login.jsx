010e983c1592b2efa21e1c83148aa81f88b03116
Pritesh lathia <72033803+pritesh-1998@users.noreply.github.com>
2024-07-22 22:47:16 +0530

    added user creation with firebase and profile upload

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db, storage } from "../../lib/firebse"
+import { doc, setDoc } from "firebase/firestore";
+import upload from "../../lib/upload";
+
 const Login = () => {
     const [image, setimage] = useState({
         file: null,
@@ -17,12 +22,34 @@ const Login = () => {
         }
 
     }
-    const handleRegister = (e) => {
+    const handleRegister = async e => {
         e.preventDefault();
-        const formdata = new FormData();
-        const { usernames, email, password } = Object.fromEntries(formdata);
-        console.log({ usernames, email, password });
-        toast.success("Account succesfully Created");
+        const formdata = new FormData(e.target);
+        const { username, email, password } = Object.fromEntries(formdata);
+        console.log({ username, email, password });
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(res.user.uid);
+            const downloadUrl = await upload(image.file);
+            console.log(downloadUrl);
+            console.log(image);
+            await setDoc(doc(db, "users", res.user.uid), {
+                username: username,
+                email: email,
+                avatar: downloadUrl,
+                id: res.user.uid,
+                blocked: [],
+            });
+            await setDoc(doc(db, "userschats", res.user.uid), {
+                chats: [],
+            });
+
+            toast.success("Account succesfully Created");
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+
+        }
     }
     return (
         <div className='login'>
@@ -37,13 +64,13 @@ const Login = () => {
             <div className="seprator"></div>
             <div className="item">
                 <h2>Create an Account</h2>
-                <form action="" onSubmit={handleRegister}>
+                <form onSubmit={handleRegister}>
                     <label htmlFor="file">
                         <img src={image.url ? image.url : "./avatar.png"} alt="" />
                         Upload Your Profile
                     </label>
-                    <input type="text" name="username" placeholder="Username" />
                     <input type="file" name="img" id="file" style={{ "display": "none" }} onChange={handleImageUpload} />
+                    <input type="text" name="username" placeholder="Username" />
                     <input type="email" name="email" placeholder="Email" />
                     <input type="password" name="password" placeholder="password" />
                     <button>Log In</button>
